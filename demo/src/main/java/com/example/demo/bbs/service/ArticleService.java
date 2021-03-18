b38c0a3118fa97701f931af94e7569216043386b
Taksungjin <tak3798@naver.com>
2021-03-18 09:28:40 +0900

    0317

--- demo/src/main/java/com/example/demo/bbs/service/ArticleService.java
+++ demo/src/main/java/com/example/demo/bbs/service/ArticleService.java
@@ -1,5 +1,16 @@
 package com.example.demo.bbs.service;
 
-public interface ArticleService {
+import java.util.List;
+import java.util.Optional;
 
+import com.example.demo.bbs.domain.Article;
+
+public interface ArticleService {
+	public Optional<Article> findById(long id);
+	public List<Article> findAll();
+	public long count();
+	public Article getOne(long id);
+	public void deleteById(long id);
+	public Article save(Article entitiy);
+	public boolean existsById(long id);
 }
--- demo/src/main/java/com/example/demo/bbs/service/ArticleServiceImpl.java
+++ demo/src/main/java/com/example/demo/bbs/service/ArticleServiceImpl.java
@@ -3,6 +3,6 @@ package com.example.demo.bbs.service;
 import org.springframework.stereotype.Service;
 
 @Service
-public class ArticleServiceImpl implements ArticleService{
+public class ArticleServiceImpl {
 
 }
